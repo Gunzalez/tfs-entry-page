b9ec237d8268e089f9802b673225e8099abb47de
segun.konibire <gunzalez@hotmail.com>
2017-11-17 11:24:24 +0000

    Fixes input retaining config ID bug

--- src/App.js
+++ src/App.js
@@ -227,7 +227,7 @@ class App extends Component {
                                         </div>
 
                                         <label>Config ID</label><span className="helper">(type in just the config ID eg. <span>2hwH09m</span>)</span>
-                                        <input id="configId" onChange={this.onChangeConfigIdHandler} name="configId" className="form-control configId" />
+                                        <input id="configId" onChange={this.onChangeConfigIdHandler} name="configId" defaultValue="" className="form-control configId" autoComplete="off" />
 
                                         <p className="error display-none">Please provide a config ID to progress</p>
                                         <button name="btn-submit" className="btn btn-default btn-submit" disabled>Launch</button>
